7f94b5817fe75af499cee2f8d4ad78c0d8c98640
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-01-23 14:25:03 -0300

    :tada: Criando projeto com Serverless e configurando o DynamoDB

---
+++ serverless.ts
@@ -0,0 +1,75 @@
+import type { AWS } from '@serverless/typescript';
+
+const serverlessConfiguration: AWS = {
+  service: 'certificateignite',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild', 'serverless-offline'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+  },
+  // import the function via paths
+  functions: {
+    generateCertificate: {
+      handler: 'src/functions/generateCertificate.handler',
+      events: [
+        {
+          http: {
+            path: 'hello',
+            method: 'get',
+            cors: true,
+          },
+        },
+      ],
+    }
+   },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+  resources: {
+    Resources: {
+      dbCerticateUsers: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "users_certificate",
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5,
+          },
+          AttributeDefinitions: [
+            {
+              AttributeName: "id",
+              AttributeType: "S",
+            }
+          ],
+          KeySchema: [
+            {
+              AttributeName: "id",
+              KeyType: "HASH",
+            }
+          ],
+        }
+      }
+    }
+  }
+};
+
+module.exports = serverlessConfiguration;
