9e072797c01ce94f2b2944c589a74355693c6901
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-01-27 13:41:00 -0300

    :sparkles: feat: Validaçao do certificado

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,11 +1,11 @@
-import { APIGatewayProxyHandler } from 'aws-lambda'
-import { document } from '../utils/dynamodbClient';
-import * as Handlebars from 'handlebars';
-import dayjs from 'dayjs';
-import { join } from 'path';
-import { readFileSync } from 'fs';
-import chromium from 'chrome-aws-lambda';
-import AWS from 'aws-sdk';
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { document } from "../utils/dynamodbClient";
+import * as Handlebars from "handlebars";
+import dayjs from "dayjs";
+import { join } from "path";
+import { readFileSync } from "fs";
+import chromium from "chrome-aws-lambda";
+import AWS from "aws-sdk";
 
 interface ICreateCertificate {
   id: string;
@@ -26,36 +26,36 @@ const compile = async (data: ITemplate) => {
 
   const html = readFileSync(filePath, "utf-8");
 
-  return Handlebars.compile(html)(data)
-}
+  return Handlebars.compile(html)(data);
+};
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-  const {
-    id,
-    name, 
-    grade,
-  } = JSON.parse(event.body) as ICreateCertificate;
+  const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
-  const response = await document.query({
-    TableName: "users_certificate",
-    KeyConditionExpression: "id = :id",
-    ExpressionAttributeValues: {
-      ":id": id,
-    }
-  }).promise();
+  const response = await document
+    .query({
+      TableName: "users_certificate",
+      KeyConditionExpression: "id = :id",
+      ExpressionAttributeValues: {
+        ":id": id,
+      },
+    })
+    .promise();
 
   const userAlreadyExists = response.Items[0];
 
   if (!userAlreadyExists) {
-    await document.put({
-      TableName: "users_certificate",
-      Item: {
-        id,
-        name,
-        grade,
-        created_at: new Date().getTime(),
-      }
-    }).promise();
+    await document
+      .put({
+        TableName: "users_certificate",
+        Item: {
+          id,
+          name,
+          grade,
+          created_at: new Date().getTime(),
+        },
+      })
+      .promise();
   }
 
   const medalPath = join(process.cwd(), "src", "templates", "selo.png");
@@ -67,7 +67,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     grade,
     medal,
     date: dayjs().format("DD/MM/YYYY"),
-  }
+  };
 
   const content = await compile(data);
 
@@ -97,19 +97,21 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   //   Bucket: "certificateignitepvn2023",
   // }).promise();
 
-  await s3.putObject({
-    Bucket: "certificateignitepvn2023",
-    Key: `${id}.pdf`,
-    ACL: "public-read",
-    Body: pdf,
-    ContentType: "application/pdf"
-  }).promise();
+  await s3
+    .putObject({
+      Bucket: "certificateignitepvn2023",
+      Key: `${id}.pdf`,
+      ACL: "public-read",
+      Body: pdf,
+      ContentType: "application/pdf",
+    })
+    .promise();
 
   return {
     statusCode: 201,
     body: JSON.stringify({
       message: "Certificado criado com sucesso!",
-      url: `https://certificateignitepvn2023.s3.amazonaws.com/${id}.pdf`
+      url: `https://certificateignitepvn2023.s3.amazonaws.com/${id}.pdf`,
     }),
   };
 };
